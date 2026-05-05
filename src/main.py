abf4bbbe11d472bd45a1df0dc9dd1e364fc20d48
22VINAYAK98 <vinayakmboormane@gmail.com>
2026-05-05 15:14:55 -0400

    Initial BDD pipeline with Docker structure

---
+++ src/main.py
@@ -0,0 +1,12 @@
+# from src.data.loader import load_data
+# from src.analysis.eda import run_eda
+
+# def main():
+#     data = load_data("data/raw")
+#     run_eda(data)
+
+def main():
+    print("abllew to run main ")
+
+if __name__ == "__main__":
+    main()
